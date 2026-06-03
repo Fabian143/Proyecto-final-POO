ecc2cea1c8b5df7b87320c5a32d8b485c054bb96
Fabian Yesid Londoño Penagos <flondono197@gmail.com>
2026-06-03 10:38:53 -0500

    Revert "Update Publicacion.java"
    This reverts commit 8c5a59671e596a6bb8f1efe83fa889d90a1fc7f7.

--- src/kernel/Publicacion.java
+++ src/kernel/Publicacion.java
@@ -1,51 +1,9 @@
 package kernel;
 
-import interfaces.Geolocalizable;
-import enums.EstadoPublicacion;
-
-import java.io.Serializable;
-
-public abstract class Publicacion
-        implements Geolocalizable, Serializable {
-
-    protected int id;
-    protected String descripcion;
-    protected String categoria;
-    protected String ubicacion;
-    protected EstadoPublicacion estado;
-
-    public Publicacion(int id,
-                       String descripcion,
-                       String categoria,
-                       String ubicacion) {
-
-        this.id = id;
-        this.descripcion = descripcion;
-        this.categoria = categoria;
-        this.ubicacion = ubicacion;
-        this.estado = EstadoPublicacion.PUBLICADO;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public EstadoPublicacion getEstado() {
-        return estado;
-    }
-
-    public void setEstado(EstadoPublicacion estado) {
-        this.estado = estado;
-    }
-
-    @Override
-    public String obtenerUbicacion() {
-        return ubicacion;
-    }
-
-    public abstract void mostrarPublicacion();
+public class Publicacion {
+	EstadoTransaccion estado=new EstadoTransaccion();
+	TiempoGeolocalizado fecha=new TiempoGeolocalizado();
+	UsuarioNormal Propietario=new UsuarioNormal();
+	Objeto objetoPublicacion=new Objeto();
+	tipoP tipo;
 }
