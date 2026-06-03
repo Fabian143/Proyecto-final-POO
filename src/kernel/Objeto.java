2c231fbe9a3d3fa6daed4c1ad999fb044a9f3b12
Fabian Yesid Londoño Penagos <flondono197@gmail.com>
2026-06-03 15:41:58 -0500

    Refactor Objeto: agregar constructor, getters/setters y métodos para gestionar fotos

--- src/kernel/Objeto.java
+++ src/kernel/Objeto.java
@@ -1,13 +1,58 @@
 package kernel;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Objeto {
-	String descripcion;
-	String condiciones;
-	String categoria;
-	/*no se como agregar las fotos podria ser con archivos que apunte hacia
-	 *  la carpeta de fotos asi que por ahora sera un arraylist de strings
-	 */
-	ArrayList<String> fotos;
+public class Objeto implements Serializable {
+    
+    private String descripcion;
+    private String condiciones;
+    private String categoria;
+    private ArrayList<String> fotos;
+
+    public Objeto(String descripcion, String condiciones, String categoria) {
+        this.descripcion = descripcion;
+        this.condiciones = condiciones;
+        this.categoria = categoria;
+        this.fotos = new ArrayList<>();
+    }
+
+    // Getters
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public String getCondiciones() {
+        return condiciones;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public ArrayList<String> getFotos() {
+        return fotos;
+    }
+
+    // Setters
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setCondiciones(String condiciones) {
+        this.condiciones = condiciones;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    // Métodos para gestionar fotos
+    public void agregarFoto(String rutaFoto) {
+        fotos.add(rutaFoto);
+    }
+
+    public void removerFoto(String rutaFoto) {
+        fotos.remove(rutaFoto);
+    }
 }
