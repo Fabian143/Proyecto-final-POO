f280819a9c1978258deee5b24d517804155f96dd
Fabian Yesid Londoño Penagos <flondono197@gmail.com>
2026-06-03 15:37:28 -0500

    Refactor UsuarioNormal: eliminar redundancia de contraseña y agregar constructor

--- src/kernel/UsuarioNormal.java
+++ src/kernel/UsuarioNormal.java
@@ -2,18 +2,45 @@ package kernel;
 
 import java.util.ArrayList;
 
-public class UsuarioNormal extends Usuario{
-	String ubicacion;
-	private String contraseña=super.contraseña;
-	ArrayList<Publicacion> publicaciones;
-	ArrayList<Reseña> reseñasComoVendedor;
-	ArrayList<Reseña> reseñasComoCliente;
-	ArrayList<Transaccion> transacciones;
-	public String getContraseña() {
-		return contraseña;
-	}
-	
-	public void setContraseña(String contraseña) {
-		this.contraseña = contraseña;
-	}
+public class UsuarioNormal extends Usuario {
+    
+    protected String ubicacion;
+    protected ArrayList<Publicacion> publicaciones;
+    protected ArrayList<Reseña> reseñasComoVendedor;
+    protected ArrayList<Reseña> reseñasComoCliente;
+    protected ArrayList<Transaccion> transacciones;
+
+    public UsuarioNormal(String nombre, String contraseña, String correo) {
+        super(nombre, contraseña, correo);
+        this.publicaciones = new ArrayList<>();
+        this.reseñasComoVendedor = new ArrayList<>();
+        this.reseñasComoCliente = new ArrayList<>();
+        this.transacciones = new ArrayList<>();
+    }
+
+    // Getters
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public ArrayList<Publicacion> getPublicaciones() {
+        return publicaciones;
+    }
+
+    public ArrayList<Reseña> getReseñasComoVendedor() {
+        return reseñasComoVendedor;
+    }
+
+    public ArrayList<Reseña> getReseñasComoCliente() {
+        return reseñasComoCliente;
+    }
+
+    public ArrayList<Transaccion> getTransacciones() {
+        return transacciones;
+    }
+
+    // Setters
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
 }
