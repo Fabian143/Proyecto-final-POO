c7e0386457af3d8a48f62bd3b2f1a9364a97580d
Fabian Yesid Londoño Penagos <flondono197@gmail.com>
2026-06-03 15:40:44 -0500

    Refactor Publicacion: agregar constructor, ID autogenerativo y getters/setters completos

--- src/kernel/Publicacion.java
+++ src/kernel/Publicacion.java
@@ -1,9 +1,57 @@
 package kernel;
 
-public class Publicacion {
-	EstadoTransaccion estado=new EstadoTransaccion();
-	TiempoGeolocalizado fecha=new TiempoGeolocalizado();
-	UsuarioNormal Propietario=new UsuarioNormal();
-	Objeto objetoPublicacion=new Objeto();
-	tipoP tipo;
+import java.io.Serializable;
+
+public class Publicacion implements Serializable {
+    
+    private static int contadorId = 2000;
+    protected int id;
+    protected EstadoTransaccion estado;
+    protected TiempoGeolocalizado fecha;
+    protected UsuarioNormal propietario;
+    protected Objeto objeto;
+    protected tipoP tipo;
+
+    public Publicacion(UsuarioNormal propietario, Objeto objeto, tipoP tipo) {
+        this.id = generarId();
+        this.propietario = propietario;
+        this.objeto = objeto;
+        this.tipo = tipo;
+        this.fecha = new TiempoGeolocalizado();
+        this.estado = new EstadoTransaccion();
+    }
+
+    private static int generarId() {
+        return contadorId++;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public EstadoTransaccion getEstado() {
+        return estado;
+    }
+
+    public TiempoGeolocalizado getFecha() {
+        return fecha;
+    }
+
+    public UsuarioNormal getPropietario() {
+        return propietario;
+    }
+
+    public Objeto getObjeto() {
+        return objeto;
+    }
+
+    public tipoP getTipo() {
+        return tipo;
+    }
+
+    // Setters
+    public void setEstado(EstadoTransaccion estado) {
+        this.estado = estado;
+    }
 }
