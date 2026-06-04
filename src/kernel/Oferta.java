0bacc54934fde7c7f091ffdc1cdc56a78fcff6d1
Fabian Yesid Londoño Penagos <flondono197@gmail.com>
2026-06-03 22:05:23 -0500

    Update Oferta.java

--- src/kernel/Oferta.java
+++ src/kernel/Oferta.java
@@ -1,32 +1,74 @@
 package kernel;
 
-import enums.EstadoOferta;
+public class Oferta {
+	
+	private TiempoGeolocalizado fecha;
+	private UsuarioNormal usuario;
+	private Objeto objetoOfrecido;
+	private Objeto objetoSolicitado;
+	private EstadoOferta estado;
+	
+	public Oferta(TiempoGeolocalizado fecha, UsuarioNormal usuario, Objeto objetoOfrecido, Objeto objetoSolicitado,
+			EstadoOferta estado) {
+		this.fecha = fecha;
+		this.usuario = usuario;
+		this.objetoOfrecido = objetoOfrecido;
+		this.objetoSolicitado = objetoSolicitado;
+		this.estado = estado;
+	}
+	
+	public void aceptarOferta() {
+	    estado = EstadoOferta.Aceptada;
+	}
+	public void cancelarOferta() {
+	    estado = EstadoOferta.Cancelada;
+	}
+	public void ofertaPendiente() {
+	    estado = EstadoOferta.Pendiente;
+	}
+	public void rechazarOferta() {
+	    estado = EstadoOferta.Rechazada;
+	}
+	
+	public TiempoGeolocalizado getFecha() {
+		return fecha;
+	}
 
-import java.io.Serializable;
-import java.time.LocalDate;
+	public void setFecha(TiempoGeolocalizado fecha) {
+		this.fecha = fecha;
+	}
 
-public class Oferta implements Serializable {
+	public UsuarioNormal getUsuario() {
+		return usuario;
+	}
 
-    protected Usuario usuario;
-    protected LocalDate fecha;
-    protected EstadoOferta estado;
+	public void setUsuario(UsuarioNormal usuario) {
+		this.usuario = usuario;
+	}
 
-    public Oferta(Usuario usuario) {
+	public Objeto getObjetoOfrecido() {
+		return objetoOfrecido;
+	}
 
-        this.usuario = usuario;
-        this.fecha = LocalDate.now();
-        this.estado = EstadoOferta.PENDIENTE;
-    }
+	public void setObjetoOfrecido(Objeto objetoOfrecido) {
+		this.objetoOfrecido = objetoOfrecido;
+	}
 
-    public Usuario getUsuario() {
-        return usuario;
-    }
+	public Objeto getObjetoSolicitado() {
+		return objetoSolicitado;
+	}
 
-    public EstadoOferta getEstado() {
-        return estado;
-    }
+	public void setObjetoSolicitado(Objeto objetoSolicitado) {
+		this.objetoSolicitado = objetoSolicitado;
+	}
 
-    public void setEstado(EstadoOferta estado) {
-        this.estado = estado;
-    }
+	public EstadoOferta getEstado() {
+		return estado;
+	}
+
+	public void setEstado(EstadoOferta estado) {
+		this.estado = estado;
+	}
+	
+	
 }
