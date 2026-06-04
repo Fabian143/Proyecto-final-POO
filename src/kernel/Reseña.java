92e9238d214a590ab9f47e7779cbbdee8bb2fd83
Fabian Yesid Londoño Penagos <flondono197@gmail.com>
2026-06-03 22:08:15 -0500

    Update Reseña.java

--- src/kernel/Reseña.java
+++ src/kernel/Reseña.java
@@ -1,17 +1,74 @@
 package kernel;
 
-
 import java.io.Serializable;
 
 public class Reseña implements Serializable {
+    
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private static int contadorId = 5000;
+    protected int id;
+    protected UsuarioNormal autor;
+    protected double calificacion;
+    protected String comentario;
+    protected TiempoGeolocalizado fecha;
 
-    private int calificacion;
-    private String comentario;
+    // Constructor completo
+    public Reseña(UsuarioNormal autor, double calificacion, String comentario) {
+        this.id = generarId();
+        this.autor = autor;
+        this.calificacion = validarCalificacion(calificacion);
+        this.comentario = comentario != null ? comentario : "";
+        this.fecha = new TiempoGeolocalizado();
+    }
 
-    public Reseña(int calificacion,
-                  String comentario) {
+    private static int generarId() {
+        return contadorId++;
+    }
 
-        this.calificacion = calificacion;
-        this.comentario = comentario;
+    // Validar que la calificación esté entre 1 y 5
+    private double validarCalificacion(double calificacion) {
+        if (calificacion < 1.0 || calificacion > 5.0) {
+            throw new IllegalArgumentException("La calificación debe estar entre 1.0 y 5.0");
+        }
+        return calificacion;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public UsuarioNormal getAutor() {
+        return autor;
+    }
+
+    public double getCalificacion() {
+        return calificacion;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public TiempoGeolocalizado getFecha() {
+        return fecha;
+    }
+
+    // Setters
+    public void setCalificacion(double calificacion) {
+        this.calificacion = validarCalificacion(calificacion);
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario != null ? comentario : "";
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Reseña{id=%d, autor=%s, calificación=%.1f, fecha=%s, comentario='%s'}",
+                id, autor.getNombre(), calificacion, fecha.getFechaFormato(), comentario);
     }
 }
