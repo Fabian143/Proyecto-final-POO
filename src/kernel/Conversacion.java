f1ba5951be02b5a55d3298a0d3ea8e9958538516
wilfredolozanor07-netizen <wilfredolozanor07@gmail.com>
2026-06-03 21:29:37 -0500

    Update Conversacion.java

--- src/kernel/Conversacion.java
+++ src/kernel/Conversacion.java
@@ -1,48 +1,21 @@
 package kernel;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Conversacion {
-	private UsuarioNormal remitente;
-	private UsuarioNormal destinatario;
-	
+public class Conversacion implements Serializable {
 
-	private ArrayList<Mensaje> mensajes;
-	
-	public Conversacion(UsuarioNormal remitente, UsuarioNormal destinatario) {
-		this.remitente = remitente;
-		this.destinatario = destinatario;
-		mensajes = new ArrayList<>();
-	}
-    
-	void añadirMensaje(Mensaje m) {
-		mensajes.add(m);
-	}
-   
-    public int cantidadMensajes() {
-        return mensajes.size();
+    private ArrayList<Mensaje> mensajes;
+
+    public Conversacion() {
+        mensajes = new ArrayList<>();
     }
-public UsuarioNormal getRemitente() {
-		return remitente;
-	}
 
-	public void setRemitente(UsuarioNormal remitente) {
-		this.remitente = remitente;
-	}
+    public void agregarMensaje(Mensaje m) {
+        mensajes.add(m);
+    }
 
-	public UsuarioNormal getDestinatario() {
-		return destinatario;
-	}
-
-	public void setDestinatario(UsuarioNormal destinatario) {
-		this.destinatario = destinatario;
-	} 
-	public ArrayList<Mensaje> getMensajes() {
-		return mensajes;
-	}
-
-	public void setMensajes(ArrayList<Mensaje> mensajes) {
-		this.mensajes = mensajes;
-	}
-	
+    public ArrayList<Mensaje> getMensajes() {
+        return mensajes;
+    }
 }
