90781b5218053d8fa7bbf253709a9ec1f51e4db4
wilfredolozanor07-netizen <wilfredolozanor07@gmail.com>
2026-06-03 21:27:50 -0500

    Update Puja.java

--- src/kernel/Puja.java
+++ src/kernel/Puja.java
@@ -1,7 +1,17 @@
 package kernel;
 
-public class Puja {
-	int monto;
-	TiempoGeolocalizado t=new TiempoGeolocalizado();
-	Long fechaHora=t.getFechaHora();
+public class Puja extends Oferta {
+
+    private double valor;
+
+    public Puja(Usuario usuario,
+                double valor) {
+
+        super(usuario);
+        this.valor = valor;
+    }
+
+    public double getValor() {
+        return valor;
+    }
 }
