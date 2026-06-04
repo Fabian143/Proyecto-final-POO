26a495c3fd1333511a66b962763c88ccf8156f14
Fabian Yesid Londoño Penagos <flondono197@gmail.com>
2026-06-04 18:38:19 -0500

    Logica de conversacion arreglada

--- src/kernel/Conversacion.java
+++ src/kernel/Conversacion.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 
 public class Conversacion implements Serializable {
 
-<<<<<<< HEAD
     private static final long serialVersionUID = 1L;
 
     private UsuarioNormal remitente;
@@ -13,44 +12,19 @@ public class Conversacion implements Serializable {
 
     private ArrayList<Mensaje> mensajes;
 
-    public Conversacion(UsuarioNormal remitente, UsuarioNormal destinatario) {
+    public Conversacion(
+            UsuarioNormal remitente,
+            UsuarioNormal destinatario) {
+
         this.remitente = remitente;
         this.destinatario = destinatario;
         this.mensajes = new ArrayList<>();
-=======
-    /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private UsuarioNormal remitente;
-	private UsuarioNormal destinatario;
-	
-	private ArrayList<Mensaje> mensajes;
-
-	
-	public Conversacion(UsuarioNormal remitente, UsuarioNormal destinatario) {
-		this.remitente = remitente;
-		this.destinatario = destinatario;
-		mensajes = new ArrayList<>();
-	}
-    
-	void añadirMensaje(Mensaje m) {
-		mensajes.add(m);
-	}
-   
-    public int cantidadMensajes() {
-        return mensajes.size();
->>>>>>> ebcae6d (Logica completa sin comentarios)
     }
-public UsuarioNormal getRemitente() {
-		return remitente;
-	}
 
-	public void setRemitente(UsuarioNormal remitente) {
-		this.remitente = remitente;
-	}
+    public void agregarMensaje(Mensaje m) {
+        mensajes.add(m);
+    }
 
-<<<<<<< HEAD
     public int cantidadMensajes() {
         return mensajes.size();
     }
@@ -78,21 +52,4 @@ public UsuarioNormal getRemitente() {
     public void setMensajes(ArrayList<Mensaje> mensajes) {
         this.mensajes = mensajes;
     }
-=======
-	public UsuarioNormal getDestinatario() {
-		return destinatario;
-	}
-
-	public void setDestinatario(UsuarioNormal destinatario) {
-		this.destinatario = destinatario;
-	} 
-	public ArrayList<Mensaje> getMensajes() {
-		return mensajes;
-	}
-
-	public void setMensajes(ArrayList<Mensaje> mensajes) {
-		this.mensajes = mensajes;
-	}
-	
->>>>>>> ebcae6d (Logica completa sin comentarios)
 }
