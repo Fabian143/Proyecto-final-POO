87af276a2fae240aa72935a436b36bb8ffd18816
Junoxzz <juan.di3go.2d@gmail.com>
2026-06-05 00:29:18 -0500

    Update OfertasRecibidas.java

--- src/gui/OfertasRecibidas.java
+++ src/gui/OfertasRecibidas.java
@@ -1,11 +1,12 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
-
-import gestion.Sistema;
-import kernel.Oferta;
-import kernel.OfertaNoValida;
-import kernel.Publicacion;
-import kernel.TruequeDirecto;
 
 public class OfertasRecibidas extends JPanel {
 
