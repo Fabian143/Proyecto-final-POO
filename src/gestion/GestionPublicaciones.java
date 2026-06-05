82d0a2d0bf30c8c345e1b4b44830aa83cdd43dfd
Junoxzz <juan.di3go.2d@gmail.com>
2026-06-05 00:02:11 -0500

    Update GestionPublicaciones.java

--- src/gestion/GestionPublicaciones.java
+++ src/gestion/GestionPublicaciones.java
@@ -2,16 +2,15 @@ package gestion;
 
 import java.util.ArrayList;
 
+import kernel.EstadosP;
 import kernel.Publicacion;
 import kernel.UsuarioNormal;
 
 public class GestionPublicaciones {
 
-    public ArrayList<Publicacion>
-    obtenerTodas() {
+    public ArrayList<Publicacion> obtenerTodas() {
 
-        ArrayList<Publicacion>
-        publicaciones =
+        ArrayList<Publicacion> publicaciones =
                 new ArrayList<>();
 
         for(UsuarioNormal usuario :
@@ -23,5 +22,24 @@ public class GestionPublicaciones {
         }
 
         return publicaciones;
+    }
+
+    public ArrayList<Publicacion>
+    obtenerPublicacionesDisponibles() {
+
+        ArrayList<Publicacion> disponibles =
+                new ArrayList<>();
+
+        for(Publicacion p :
+                obtenerTodas()) {
+
+            if(p.getEstado()
+                    != EstadosP.CANCELADO) {
+
+                disponibles.add(p);
+            }
+        }
+
+        return disponibles;
     }
 }
