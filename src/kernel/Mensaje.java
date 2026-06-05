96ecb4738cccb78ed3b32e72ebeb40df16749d56
Junoxzz <juan.di3go.2d@gmail.com>
2026-06-05 08:54:55 -0500

    Update Mensaje.java

--- src/kernel/Mensaje.java
+++ src/kernel/Mensaje.java
@@ -1,6 +1,11 @@
 package kernel;
 
-public class Mensaje {
+import java.io.Serializable;
+
+
+public class Mensaje implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
 	private String contenido;
 	private TiempoGeolocalizado fecha;
 	private UsuarioNormal remitente;
